5b79511f225e4b665173b99d5cfb62c6c698b2d3
Jashim Gafoor <jashim@RCKR.COM>
2019-07-17 19:38:38 +0530

    initial commit

---
+++ DobApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace DobApp
+{
+    class Program
+    {
+
+        private static void AgeCalculator(DateTime dob)
+        {
+            Console.WriteLine("Age: {0} Year(s), {1} Month(s) and {2} Day(s)", Math.Abs(DateTime.Now.Year - dob.Year), Math.Abs(DateTime.Now.Month - dob.Month), Math.Abs(DateTime.Now.Day - dob.Day));
+            Console.WriteLine("Your Born day of the week: {0}", dob.DayOfWeek);
+        }
+
+        public static void Main(string[] args)
+        {
+            do
+            {
+                Console.Write("\nEnter your DOB(Use '/' as seperator): ");
+                String dobStr = Console.ReadLine();
+                DateValidation(dobStr);
+
+                Console.WriteLine();
+                Console.Write("Do you want to continue (y/n)?");
+
+            } while (Console.ReadKey().KeyChar != 'n');
+        }
+
+        private static void DateValidation(string dobStr)
+        {
+            try
+            {
+                DateTime dob = DateTime.ParseExact(dobStr, "dd/MM/yyyy", null);
+
+                if (dob > DateTime.Now)
+                    Console.WriteLine("Invalid Date!");
+
+                else
+                {
+                    AgeCalculator(dob);
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid Date!");
+
+            }
+        }
+    }
+}
