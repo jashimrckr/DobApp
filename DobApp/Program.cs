684be7587f8e4ea3721bc3c112d3caaef904152e
Jashim Gafoor <jashim@RCKR.COM>
2019-07-18 13:20:00 +0530

    Remove ignored files

--- DobApp/Program.cs
+++ DobApp/Program.cs
@@ -8,30 +8,22 @@ namespace DobApp
 
         private static void AgeCalculator(DateTime dob)
         {
-            Console.WriteLine("Age: {0} Year(s), {1} Month(s) and {2} Day(s)", Math.Abs(DateTime.Now.Year - dob.Year), Math.Abs(DateTime.Now.Month - dob.Month), Math.Abs(DateTime.Now.Day - dob.Day));
+
+            int year = DateTime.Now.Year - dob.Year;
+            //Console.WriteLine(DateTime.Now.DayOfYear);
+           
+            //Console.WriteLine(DateTime.Today);
+            //Console.WriteLine(dob.DayOfYear);
+            if (DateTime.Now.DayOfYear < dob.DayOfYear)
+                year = year - 1;
+
+            Console.WriteLine("Age: {0} Year(s), {1} Month(s) and {2} Day(s)", year, Math.Abs(DateTime.Now.Month - dob.Month), Math.Abs(DateTime.Now.Day - dob.Day));
             Console.WriteLine("Your Born day of the week: {0}", dob.DayOfWeek);
         }
 
         private static void DateValidation(string dobStr)
         {
-            //try
-            //{
-            //    DateTime dob = DateTime.ParseExact(dobStr, "dd/MM/yyyy", null);
-
-            //    if (dob > DateTime.Now)
-            //        Console.WriteLine("Invalid Date!");
-
-            //    else
-            //    {
-            //        AgeCalculator(dob);
-            //    }
-            //}
-            //catch (FormatException)
-            //{
-            //    Console.WriteLine("Invalid Date!");
-
-            //}
-
+         
             string format = "dd/MM/yyyy";
             DateTime dateTime;
             if (DateTime.TryParseExact(dobStr, format, CultureInfo.InvariantCulture,DateTimeStyles.None, out dateTime))
